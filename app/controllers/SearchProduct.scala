1314dae88579922ec3bc3727e90b47329f6778d6
Mark <mark.a.kelly2@gmail.com>
2021-09-16 16:46:31 +0100

    complete list user stub

--- app/controllers/SearchProduct.scala
+++ app/controllers/SearchProduct.scala
@@ -13,12 +13,33 @@ class SearchProduct @Inject()(cc: ControllerComponents) extends AbstractControll
 
   lazy val logger: Logger = Logger(this.getClass)
 
-  def searchProduct(category: Option[String],
-                    price: Option[String],
-                    name: Option[String] ): Action[AnyContent] = Action { implicit request =>
+  def searchProduct(price: Option[String],
+                    category: Option[String],
+                    name: Option[String]): Action[AnyContent] = Action { implicit request =>
 
-      logger.info(s"  testHeader: ${testHeaderFromRequest()}")
+    logger.info(s"request params: category:[$category], price:[$price] testHeader: ${testHeaderFromRequest()}")
 
+    lazy val failOnValidParams: Option[Result] = {
+
+      val badRequestResponse = BadRequest(Json.toJson(ErrorResponse(s"An invalid parameter was passed")))
+
+      val cat = category match {
+        case Some("Main") | Some("Fries") | Some("Drink") | None => None
+        case _ =>
+          Some(badRequestResponse)
+      }
+      val prc = price match {
+        case Some(prc) if !prc.matches("^\\d+(.\\d{1,2})?$") =>
+          logger.warn(s"an invalid price parameter was submitted: $price")
+          Some(badRequestResponse)
+        case _ =>
+          None
+      }
+
+      cat orElse prc
+    }
+
+    val result: Result = {
       val products = Seq(
         Product(
           id = UUID.randomUUID().toString,
@@ -36,15 +57,17 @@ class SearchProduct @Inject()(cc: ControllerComponents) extends AbstractControll
 
       testHeaderFromRequest() match {
         case Some("NOT_FOUND") =>
-          NotFound(Json.toJson(ErrorResponse("User not found")))
+          NotFound(Json.toJson(ErrorResponse("A product could not be found with the information supplied")))
         case Some("FORBIDDEN") =>
           Forbidden(Json.toJson(ErrorResponse("User not authorised to access this resource")))
         case Some("SERVER_ERROR") =>
           InternalServerError(Json.toJson(ErrorResponse("An Internal Server Error Occurred")))
         case None | Some("DEFAULT") | _ =>
           Ok(Json.toJson(products))
-
+      }
     }
+
+    failOnValidParams.fold(result)(validationResult => validationResult)
   }
 
   private def testHeaderFromRequest()(implicit req: Request[AnyContent]): Option[String] = {
